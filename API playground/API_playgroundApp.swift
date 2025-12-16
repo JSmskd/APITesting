9e3221e54a5f11f371edd56e4bf338f67e755a15
Jay <78168583+JSmskd@users.noreply.github.com>
2025-12-16 10:14:07 -0600

    Initial Commit

---
+++ API playground/API_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_playgroundApp.swift
+//  API playground
+//
+//  Created by John Sencion on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct API_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

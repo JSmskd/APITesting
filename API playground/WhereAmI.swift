98189362fee72efb72b5dc1de9e93e5082847779
Jay <78168583+JSmskd@users.noreply.github.com>
2026-01-08 11:15:39 -0600

    third API

--- API playground/WhereAmI.swift
+++ API playground/WhereAmI.swift
@@ -10,14 +10,15 @@ import SwiftUI
 //https://api.myip.com
 struct WhereAmI: View {
     var objecta:ip = .init()
-    var objectb:ipcountry = .init()
+    var objectb:ipcode = .init()
+    var objectc:codecountry = .init()
     @State var userAdress: String = ""
     @State var country: String = ""
     @State var cc: String = ""
     var body: some View {
         Text("IPAdress - \(userAdress)")
-        Text("countrty - \(country)")
         Text("country ISO code - \(cc)")
+        Text("countrty - \(country)")
         Button("THIS WILL GET YOUR IP") {
             var b = objecta.makeRequest(resource: "")
             global.session.dataTask(with: b) { data, urlr, e in
@@ -26,9 +27,9 @@ struct WhereAmI: View {
                 }
 //                print(data)
                 if let data = data, let data = try? JSONSerialization.jsonObject(with: data) as? [String: Any]  {
-                    print(data)
+//                    print(data)
                     userAdress = data["ip"] as? String ?? userAdress
-                    country = data["country"] as? String ?? country
+//                    country = data["country"] as? String ?? country
 //                    cc = data["cc"] as? String ?? cc
                     if let adress = data["ip"] as? String {
                         var bb = objectb.makeRequest(resource: adress)
@@ -37,12 +38,46 @@ struct WhereAmI: View {
                                 print(e)
                             }
 //                        print(data)
+                            
                             if let data = data, let data = try? JSONSerialization.jsonObject(with: data) as? [String: Any]  {
-                                print(data)
+//                                print(data)
                                 cc = data["country"] as? String ?? cc
-//                            userAdress = data["ip"] as? String ?? userAdress
-//                            country = data["country"] as? String ?? country
-//                            cc = data["cc"] as? String ?? cc
+                                    var bbb = objectc.makeRequest(resource: "alpha/\(cc)")
+                                    global.session.dataTask(with: bbb) { data, urlr, e in
+                                        if let e = e {
+                                            print(e)
+                                        }
+//                                    print(data)
+                                        if let data = data {
+//                                            print(data)
+//                                            print(String(data:data,encoding: .utf8))
+                                            if let oldStri = String(data:data,encoding: .utf8) {
+                                                //                                                print(oldStri)
+                                                let can = JSONSerialization.isValidJSONObject(data)
+                                                let newStri = ("\(can ? "":"{ \"\":")\(oldStri)\(can ? "":"}")")
+                                                //                                                print("\(can) : \n \(newStri)")
+                                                //                                                print(newStri)
+                                                if let data = newStri.data(using: .utf8) {
+                                                    print("worded")
+                                                    if let data = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
+                                                        print("jsonified")
+                                                        let data = (data.keys == (["":""] as [String:Any]).keys ? data[""] ?? "ERROR" : data as Any)
+//                                                        print(data)
+                                                        if let data = data as? [[String : Any]] {
+                                                            print("made into stem parts")
+                                                            if let data = data.first {
+                                                                if let data = data["name"] as? [String:Any] {
+                                                                    if let data = data["common"] {
+                                                                        country = data as? String ?? country
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }.resume()
                             }
                         }.resume()
                     }
@@ -50,8 +85,14 @@ struct WhereAmI: View {
             }.resume()
         }
     }
-    
-    struct ipcountry:api {
+    struct codecountry:api {
+        var token: String = ""
+        
+        var url : String = "https://restcountries.com:443"
+        var apiModifier:String = "v3.1"
+        init(){}
+    }
+    struct ipcode:api {
         var token: String = ""
         
         var url : String = "https://api.country.is:443"
@@ -65,7 +106,7 @@ struct WhereAmI: View {
         var url : String = "https://api.myip.com:443"
         var apiModifier:String = ""
         init(){}
-    }
+    }//https://restcountries.com/v3.1/alpha/US
 
 }
 
